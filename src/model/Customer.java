13749268c4c3c9daed4cd6cc6a7c26281cdaaf39
Carefulrogue <carefulrogue@gmail.com>
2020-11-12 13:24:21 -0600

    added deposit/withdrawl functionality.

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -51,7 +51,7 @@ public class Customer extends User {
 		while (!exitFlag) {
 		System.out.println("Your accounts:\n"+seeAccounts()
 				+ "What action do you wish to take?\n" + "(1) Request to open a new account\n"
-				+ "(2) View Balance of an account\n" + "(3) Make transfer\n" + "(4) Accept transfer\n" + "(5) Exit");
+				+ "(2) View Balance of an account\n" + "(3) Make transfer\n" + "(4) Accept transfer\n" + "(5) Withdraw or Deposit money" + "(6) Exit");
 		if (scan.hasNextInt())
 			input=scan.nextInt();
 		else
@@ -72,10 +72,14 @@ public class Customer extends User {
 			break;
 		}//end of case
 		case 4: {
-			confirmTransfer(scan);//accept/reject transfers?
+			confirmTransfer(scan);//this is messy... but done.
 			break;
 		}//end of case
 		case 5: {
+			depositAndWithdrawal(scan);//make withdraw/deposits
+			break;
+		}//end of case
+		case 6: {
 			//exit
 			exitFlag=true;
 			break;
@@ -87,9 +91,70 @@ public class Customer extends User {
 		
 		}//end of while loop
 		
-		
-		
 	}//end of option
+	
+	private void depositAndWithdrawal(Scanner scan) {
+		Connection conn = cu.getConnection();
+		System.out.println("Make a deposit?");
+		if (scan.next().equalsIgnoreCase("Y")) {
+			System.out.println("Enter account number to deposit to:");
+			int number=scan.nextInt();
+			System.out.println("Enter ammount:");
+			double ammount = scan.nextDouble();
+			PreparedStatement fetch;
+			try {
+				fetch = conn.prepareStatement("select balance from accounts where accountnumber = ?");
+				fetch.setInt(1, number);
+				double retrievedBal=-1;
+				ResultSet rs =fetch.executeQuery();
+				if (!rs.next())
+					return;
+				retrievedBal=rs.getDouble("balance");
+				PreparedStatement alter = conn.prepareStatement("update accounts set balance = ? where accountnumber = ?");
+				alter.setDouble(1, (retrievedBal+ammount));
+				alter.setInt(2, number);
+				alter.execute();
+				System.out.println("Funds added.  Returning to previous menu...");
+
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}//end of catch
+		}//end of if
+		
+		System.out.println("Withdraw funds?");
+		if (scan.next().equalsIgnoreCase("Y")) {
+			System.out.println("Enter account number to withdraw from:");
+			int number=scan.nextInt();
+			System.out.println("Enter ammount:");
+			double ammount = scan.nextDouble();
+			PreparedStatement fetch;
+			try {
+				fetch = conn.prepareStatement("select balance from accounts where accountnumber = ?");
+				fetch.setInt(1, number);
+				double retrievedBal=-1;
+				ResultSet rs =fetch.executeQuery();
+				if (!rs.next())
+					return;
+				retrievedBal=rs.getDouble("balance");
+				PreparedStatement alter = conn.prepareStatement("update accounts set balance = ? where accountnumber = ?");
+				alter.setDouble(1, (retrievedBal-ammount));
+				
+				alter.setInt(2, number);
+				alter.execute();
+				System.out.println("Funds removed.  Returning to previous menu...");
+			
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}//end of catch
+		}//end of if
+		
+		else {
+			System.out.println("Returning to homepage...");
+		}
+		
+	}//end of depositAndWithdrawl
 	
 	private void confirmTransfer(Scanner scan) {
 		//for all accounts
@@ -121,7 +186,7 @@ public class Customer extends User {
 					fetch = conn.prepareStatement("select balance from accounts where accountnumber = ?");
 					fetch.setInt(1, temp2);
 					rs = fetch.executeQuery();
-					double oldBalance=0;
+					double oldBalance=-1;
 					if (rs.next()) {
 						oldBalance = rs.getDouble("balance");
 					}//end of if
@@ -131,22 +196,69 @@ public class Customer extends User {
 					fetch2.setInt(2,temp2);
 					rs = fetch2.executeQuery();
 					if (rs.next()) {
-						oldBalance += rs.getDouble("ammount");
+						oldBalance = rs.getDouble("ammount")+oldBalance;
 					}//end of if
-					
+					System.out.println("odbalance:" +oldBalance);
 					
 					PreparedStatement submit = conn.prepareStatement("update transactions set accepted = true where sender = ? and receiver = ?;"
 							+ "update accounts set balance = ? where accountnumber = ?;");
 					submit.setInt(1, temp);
 					submit.setInt(2, temp2);
-					submit.setDouble(3, oldBalance);//need to fetch the value of the transaction and the user
+					submit.setDouble(3, oldBalance);
 					submit.setInt(4, temp2);
+					submit.execute();
+				}//end of if
+				else {
+					//System.out.println("Returning to previous menu...");
+					//exitFlag=true;
+				}//end of else
+				System.out.println("Reject one of these?");
+				
+				
+				if (scan.next().equalsIgnoreCase("Y")) {
+					//need to kick the money back to the sender.
+					//delete the transaction record.
+					
+					System.out.println("Enter sending account number:");
+					int temp = scan.nextInt();
+					System.out.println("Enter receiving account number:");
+					int temp2 = scan.nextInt();
+					
+					fetch = conn.prepareStatement("select balance from accounts where accountnumber = ?");
+					fetch.setInt(1, temp);
+					rs = fetch.executeQuery();
+					
+					
+					
+					double oldBalance=-1;
+					if (rs.next()) {
+						oldBalance = rs.getDouble("balance");
+					}//end of if
+					
+					PreparedStatement fetch2 = conn.prepareStatement("select ammount from transactions where sender = ? and receiver = ?");
+					fetch2.setInt(1,temp);
+					fetch2.setInt(2,temp2);
+					rs = fetch2.executeQuery();
+					if (rs.next()) {
+						oldBalance = rs.getDouble("ammount")+oldBalance;
+					}//end of if
+					
+					
+					
+					PreparedStatement submit = conn.prepareStatement("delete from transactions where sender = ? and receiver = ?;"
+							+ "update accounts set balance = ? where accountnumber = ?;");
+					submit.setInt(1, temp);
+					submit.setInt(2, temp2);
+					submit.setDouble(3, oldBalance);//need to fetch the value of the transaction and the user
+					submit.setInt(4, temp);
+					submit.execute();
 					
 				}//end of if
 				else {
 					System.out.println("Returning to previous menu...");
 					exitFlag=true;
 				}//end of else
+				
 			}//end of while
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -185,7 +297,7 @@ public class Customer extends User {
 				e1.printStackTrace();
 			}//end of catch
 			
-			if (validate(senderbalance,balance)) {//I actually don't know if validate would return a good answer there, so ... damnit
+			if (validate(senderbalance,balance)) {//I actually don't know if validate would return a good answer there, so ... 
 				System.out.println("Legal balance accepted.");
 				try {
 					
@@ -237,6 +349,8 @@ public class Customer extends User {
 	
 	
 	private String seeAccounts() {
+		
+		collectAccounts();
 		String output="";
 		if (accounts.isEmpty())
 			return "No accounts attatched to this account!\nTalk to our bankers to setup one!";
